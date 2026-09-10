5ac1d224f982cbf5b7aaa8a69028979e6bcecbee
shalom <shalomamaka2000@gmail.com>
2026-09-10 20:02:31 +0100

    modified PwdGen.jav

--- src/main/java/havillah_backend/PasswordGenerator.java
+++ src/main/java/havillah_backend/PasswordGenerator.java
@@ -9,7 +9,7 @@ public class PasswordGenerator {
         BCryptPasswordEncoder encoder =
                 new BCryptPasswordEncoder();
 
-        String password = "NewAdmin123";
+        String password = "NwwAdmin123";
 
         String encodedPassword =
                 encoder.encode(password);
